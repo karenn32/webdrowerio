8721127b04a11ef9cf5828535fda9756d9a26482
Karen Kododian <karenn32@gmail.com>
2024-09-12 18:19:25 +0200

    docker

--- wdio.conf.js
+++ wdio.conf.js
@@ -135,7 +135,7 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: ['allure'],
 
 
 
